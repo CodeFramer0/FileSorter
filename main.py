49b8faa595d13e1f9a2dd48a94bdfc9d33379630
batman22827 <ilya.demchenko00@mail.ru>
2024-07-11 11:16:25 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import shutil
 
 
 
-source_folder = "sorting"  # Замените на путь к вашей папке для файлов на сортировку
-sorted_folder = "sorted" # Замените на путь к вашей папке для отсортированных файлов
+SOURCE_FOLDER = "sorting"
+SORTED_FOLDER = "sorted"
 
 def sort_files(source_folder):
     for filename in os.listdir(source_folder):
@@ -18,7 +18,7 @@ def sort_files(source_folder):
             directories = parts[:-1]
             
             
-            new_directory_path = os.path.join(sorted_folder, *directories)
+            new_directory_path = os.path.join(SORTED_FOLDER, *directories)
             new_file_path = os.path.join(new_directory_path, file_name)
             
             os.makedirs(new_directory_path, exist_ok=True)
@@ -28,4 +28,4 @@ def sort_files(source_folder):
 
 
 if __name__ == '__main__':
-    sort_files(source_folder)
+    sort_files(SOURCE_FOLDER)
